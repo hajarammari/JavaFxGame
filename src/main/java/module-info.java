79891da99fb67e3274098a69b543d9d89b97b8c8
hajar <hajarammaari@gmail.com>
2023-06-03 14:19:28 +0100

    module.info fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.javafxgame {
     requires javafx.fxml;
     requires java.sql;
     requires org.apache.poi.ooxml;
-    requires gson;
+    requires com.google.gson;
     requires json.simple;
 
     opens com.example.javafxgame.entities to gson;
